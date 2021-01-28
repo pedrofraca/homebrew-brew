64775b48e64920e94eb0d5b06f72664833e480a0
Pedro Fraca <pedro.fraca@gmail.com>
2021-01-28 23:43:48 +0100

    Formatting cask

--- Casks/scrcpyui.rb
+++ Casks/scrcpyui.rb
@@ -1,10 +1,12 @@
 cask "scrcpyui" do
   version "1.0"
-  desc "Small project to provide UI access for the scrcpy tool in macOS"
-  homepage "https://github.com/pedrofraca/scrcpyui"
-  url "https://github.com/pedrofraca/scrcpyui/releases/download/v1.0/scrcpyui.dmg"
   sha256 "0f9bbac7321415543a03665f1aa37aa4411ff3d5853d807c1dfa6c66be186051"
 
-  depends_on "scrcpy"
+  url "https://github.com/pedrofraca/scrcpyui/releases/download/v1.0/scrcpyui.dmg"
+  name "scrcpyui"
+  desc "Small project to provide UI access for the scrcpy tool"
+  homepage "https://github.com/pedrofraca/scrcpyui/"
+
   app "scrcpyui.app"
+
 end
