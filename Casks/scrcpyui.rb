3be40ebbbc321efbbd62db9406e32d1e70ab35de
Pedro Fraca <pedro.fraca@gmail.com>
2021-01-28 23:32:28 +0100

    Remove license

--- Casks/scrcpyui.rb
+++ Casks/scrcpyui.rb
@@ -4,7 +4,6 @@ cask "scrcpyui" do
   homepage "https://github.com/pedrofraca/scrcpyui"
   url "https://github.com/pedrofraca/scrcpyui/releases/download/v1.0/scrcpyui.dmg"
   sha256 "0f9bbac7321415543a03665f1aa37aa4411ff3d5853d807c1dfa6c66be186051"
-  license "GPL-3.0-only"
 
   depends_on "scrcpy"
   app "scrcpyui.app"
