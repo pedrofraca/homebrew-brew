9e683f3ec6d489f6490473efffbcf1feb58b4b91
Pedro Fraca <pedro.fraca@gmail.com>
2021-01-28 23:27:15 +0100

    Adding scrcpy Cask

---
+++ Casks/scrcpyui.rb
@@ -0,0 +1,11 @@
+cask "scrcpyui" do
+  version "1.0"
+  desc "Small project to provide UI access for the scrcpy tool in macOS"
+  homepage "https://github.com/pedrofraca/scrcpyui"
+  url "https://github.com/pedrofraca/scrcpyui/releases/download/v1.0/scrcpyui.dmg"
+  sha256 "0f9bbac7321415543a03665f1aa37aa4411ff3d5853d807c1dfa6c66be186051"
+  license "GPL-3.0-only"
+
+  depends_on "scrcpy"
+  app "scrcpyui.app"
+end
